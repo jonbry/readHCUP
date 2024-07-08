5931355de660e02f64626ebdcb9b9b936a34e918
Jon <jon@jonbryant.co>
2024-07-08 15:52:12 -0400

    Added files for NIS 2016-2018. Need to update DATASET.R and tests

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,7 +1,14 @@
 ## code to prepare `DATASET` dataset goes here
 supported_datasets <- list(data= c("NIS 2019", "NIS 2020"), dataset_file_name = c("NIS_2019_CORE_V2", "NIS_2020_CORE_V2"))
 
-# NIS 2019 Dataset Features
+# NIS 2016 Datasets
+NIS_2016_widths <- readr::read_csv("data-raw/NIS_2016_widths.csv")
+NIS_2016_descriptions <- readr::read_csv("data-raw/NIS_2016_descriptions.csv")
+NIS_2016_na <- readr::read_csv("data-raw/NIS_2016_na.csv", col_names = TRUE, col_types = list(readr::col_character()), na = "")
+NIS_2016_na <-
+  NIS_2016_na |> tibble::add_row(na_vec = "")
+
+# NIS 2019 Datasets
 NIS_2019_widths <- readr::read_csv("data-raw/NIS_2019_widths.csv")
 NIS_2019_descriptions <- readr::read_csv("data-raw/NIS_2019_descriptions.csv")
 NIS_2019_na <- readr::read_csv("data-raw/NIS_2019_na.csv", col_names = TRUE, col_types = list(readr::col_character()), na = "")
@@ -9,10 +16,7 @@ NIS_2019_na <-
   NIS_2019_na |> tibble::add_row(na_vec = "")
 NIS_2019_corrected <- readr::read_csv("data-raw/NIS_2019_PCLASS_ORPROC.csv", col_select = c("KEY_NIS", "PCLASS_ORPROC"))
 
-<<<<<<< HEAD
-# NIS 2020 Dataset Features
-=======
->>>>>>> 2a7c168ee2ecb015596e6949b20205b4142f5cd9
+# NIS 2020 Datasets
 NIS_2020_widths <- readr::read_csv("data-raw/NIS_2020_widths.csv")
 NIS_2020_descriptions <- readr::read_csv("data-raw/NIS_2020_descriptions.csv")
 NIS_2020_na <- readr::read_csv("data-raw/NIS_2020_na.csv", col_names = TRUE, col_types = list(readr::col_character()), na = "")
